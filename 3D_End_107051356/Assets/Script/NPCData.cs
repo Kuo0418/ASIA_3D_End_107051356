a560af7ec4bca55d2d927c380a15f811c227eb8e
Kuo0418 <71679871+Kuo0418@users.noreply.github.com>
2021-01-04 19:17:30 +0800

    1/3

---
+++ 3D_End_107051356/Assets/Script/NPCData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NPC 資料" , menuName = "Kuo/NPC 資料")]
+public class NPCData : MonoBehaviour
+{
+    [Header("第一段對話") , TextArea(1,5)]
+    public string dialougA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialougC;
+
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已取得項目數量")]
+    public int countCurrent;
+}
